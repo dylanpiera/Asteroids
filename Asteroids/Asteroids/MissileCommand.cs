ab3ac852df113fac3f1722af18ffdddfb9af2b31
bpijls <b.pijls@hva.nl>
2015-05-17 15:21:30 +0200

    Stap 0: TentamenStart

---
+++ Asteroids/Asteroids/MissileCommand.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Asteroids : GameEnvironment
+    {
+
+        public Asteroids()
+        {
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            screen = new Point(800, 600);
+            SetFullScreen(false);
+            IsMouseVisible = true;
+
+            // TODO: use this.Content to load your game content here      
+
+        }
+
+    }
+}
