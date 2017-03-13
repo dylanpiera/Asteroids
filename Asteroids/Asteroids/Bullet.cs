c46fc3f139fdc4ef3971179e3e50e4229b6d4042
SoaringDylan <dylan@fampiera.nl>
2017-03-13 17:21:33 +0100

    Step 3

---
+++ Asteroids/Asteroids/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+    class Bullet : SpriteGameObject
+    {
+        public Bullet(Vector2 startPos, Vector2 startSpe, string assetname = "spr_bullet", int layer = 10, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+            this.Position = startPos;
+            this.Velocity = startSpe;
+        }
+    }
+}
--- Asteroids/Asteroids/PlayingState.cs
+++ Asteroids/Asteroids/PlayingState.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +10,27 @@ namespace Asteroids
     class PlayingState : GameObjectList
     {
         SpaceShip spaceShip;
+        GameObjectList bullets;
 
         public PlayingState()
         {
             spaceShip = new SpaceShip();
+            bullets = new GameObjectList();
 
             this.Add(new SpriteGameObject("spr_background"));
+            this.Add(bullets);
             this.Add(spaceShip);
+            
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if(inputHelper.KeyPressed(Keys.Space))
+            {
+                this.bullets.Add(new Bullet(this.spaceShip.Position,this.spaceShip.AngularDirection * 300));
+            }
         }
     }
 }
